5310523a72a9840f350c34b2f2d44646facf7bcd
rerun1129 <84177337+rerun1129@users.noreply.github.com>
2021-06-29 17:38:44 +0900

    Default Changelist

---
+++ src/Run1.java
@@ -0,0 +1,10 @@
+import outPakage.Variable1;
+
+public class Run1 {
+    public static void main(String[] args) {
+        Variable2 p = new Variable2();
+        p.method1();
+        p.method2();
+        p.method4(9000);
+    }
+}
---
+++ src/Variable2.java
@@ -0,0 +1,29 @@
+public class Variable2 {
+
+    int num1 = 100;
+    int num2 = 200;
+    final int num3 = 300;
+
+
+    public void method1(){
+
+        int num2 = 150;
+        int num4 = 250;
+
+        System.out.println("전역 변수 :" + num1);
+        System.out.println("지역 변수 :" + num2);
+        System.out.println("상수 :" + num3);
+
+    }
+    public void method2(){
+
+        num1 = 123;
+        System.out.println("변경 확인 :" + num1);
+        //num3 = 456;
+
+    }
+    public void method4(int hw){
+        int lh= 9;
+        System.out.println("일당 :" + lh*hw);
+    }
+}
