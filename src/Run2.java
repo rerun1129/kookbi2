aff8bb02f175305572fec9c052bb70ab066fa589
rerun1129 <84177337+rerun1129@users.noreply.github.com>
2021-07-07 16:40:29 +0900

    Default Changelist

--- src/Run2.java
+++ src/Run2.java
@@ -1,26 +1,15 @@
 abstract class Run2 {
 
-    static int a = 10;
-
-
     public static void main(String[] args) {
-        fieldTest();
+       Dog pup1 = new Dog("삼돌이","진도");
+       Dog pup2 = new Dog();
+       pup2.setName("만식이");
+        pup1.bark();
+        pup2.play();
+        pup1.play();
+        Dog.checkBob();
+        pup1.printDog();
+        pup2.printDog();
 
-
-    } public void test1 () {
-            new object().name = "철수";
-            new object().gender = 'M';
-            new object().age = 10;
-            //new object().tel = "010";
-        }
-        public static void fieldTest () {
-            FieldTest ft1 = new FieldTest();
-
-            System.out.println(ft1.PI);
-
-            FieldTest ft2 = new FieldTest();
-
-            System.out.println(ft2.PI);
-
-        }
+    }
 }
