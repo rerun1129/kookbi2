b7ee136550b3016979dd6e6a705fc975b9817f15
rerun1129 <84177337+rerun1129@users.noreply.github.com>
2021-07-16 23:19:30 +0900

    파일 받는거로 고침

--- src/com/kh/bank/controller/BankManager.java
+++ src/com/kh/bank/controller/BankManager.java
@@ -4,21 +4,22 @@ package com.kh.bank.controller;
 import com.kh.bank.model.vo.Bank;
 import com.kh.bank.view.BankMenu;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class BankManager {
 
-    int a = 100;
+    BankMenu bm = new BankMenu();
+
+    int count = 5;
 
 
-    public void printInfo() {
-        System.out.println("1. 조회");
-        System.out.println(">>> 잔액 : " + a + "원");
+    public void printInfo(Bank[] data, int i) {
+
+        System.out.println(">>> 잔액 : " + data[i].getBalance() + "원");
+
     }
 
-    public void logIn(Bank[] data, int n) {
+    public int logIn(Bank[] data, int n) {
 
         Scanner in = new Scanner(System.in);
         System.out.print("계좌번호 : ");
@@ -36,15 +37,14 @@ public class BankManager {
                 System.out.print("계좌번호 : ");
                 ac = in.next();
             }
-
         }
         int m = 0;
         while (m < 5) {
             System.out.print("비밀번호 : ");
             String pw = in.next();
             if (data[i].getPw().equals(pw)) {
-                System.out.println(data[i].getName()+"님 환영합니다.");
-                return;
+                System.out.println(data[i].getName() + "님 환영합니다.");
+                break;
             } else {
                 System.out.println("비밀번호를 " + (m + 1) + "번 잘못 입력하셨습니다.");
                 m++;
@@ -52,9 +52,76 @@ public class BankManager {
             if (m == 5) {
                 System.out.println("비밀번호를 5회 잘못 입력하셨기 때문에 프로그램을 종료합니다.");
                 System.exit(1);
+
             }
+
+        }
+        return i;
+    }
+
+
+    public void withdrawal(Bank[] data, int i) {
+        System.out.println("얼마를 출금하시겠습니까?");
+        Scanner sc = new Scanner(System.in);
+        int wd = sc.nextInt();
+
+        if (data[i].getBalance() >= wd) {
+            System.out.println(wd + "원을 출금하였습니다.");
+            System.out.println("잔액 : " + (data[i].getBalance() - wd) + "원");
+            data[i].setBalance(data[i].getBalance() - wd);
+        } else {
+            System.out.println("잔액이 부족합니다!");
         }
     }
+
+    public void remittance(Bank[] data, int i) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(" 현재 잔액 :  " + data[i].getBalance() + "원");
+
+        System.out.print(" 입금받으실 분의 계좌번호를 적어주십시오 :  ");
+        String account = sc.next();
+
+        int j;
+        for (j = 0; j < count; ) {
+            if (data[j].getAccount().equals(account)) {
+                break;
+            } else if (!data[j].getAccount().equals(account)) {
+                j++;
+            }
+            if (j == count) {
+                System.out.println("계좌번호 오류.");
+                j = 0;
+                System.out.print("입금받으실 분의 계좌번호를 적어주십시오 : ");
+                account = sc.next();
+            }
+        }
+
+
+        System.out.print(data[j].getName() + "님 께 얼마를 송금하시겠습니까?:   ");
+        int money = sc.nextInt();
+
+        if (money < data[i].getBalance()) {
+            System.out.println(data[j].getName() + " 님께 " + money + " 원 송금하셨습니다. ");
+            data[i].setBalance(data[i].getBalance() - money);
+            data[j].setBalance(data[j].getBalance() + money);
+        } else {
+            System.out.println("잔액이 부족합니다.");
+        }
+        System.out.println(" 잔액 :  " + data[i].getBalance() + " 원");
+    }
+
+    public void deposit(Bank[] data, int i) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(">>> 얼마를 입금하시겠습니까?");
+        int price = sc.nextInt();
+        data[i].setBalance(data[i].getBalance() + price);
+        System.out.println(price + "원을 입금하였습니다.");
+        System.out.println("잔액 : " + data[i].getBalance() + "원");
+    }
 }
+
+
+
 
 
--- src/com/kh/bank/view/BankMenu.java
+++ src/com/kh/bank/view/BankMenu.java
@@ -1,7 +1,5 @@
 package com.kh.bank.view;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 import com.kh.bank.controller.BankManager;
@@ -10,25 +8,26 @@ import com.kh.bank.model.vo.Bank;
 public class BankMenu {
 
 
-
     public static void display() {
 
         Bank[] bank = new Bank[5];
 
 
         bank[0] = new Bank("3643", "4875414785221", 1000000L, "고윤석");
-        bank[1] = new Bank("2680", "299559469724", 1000000L, "이승병");
-        bank[2] = new Bank("3606", "3920348293843", 1000000L, "박희진");
-        bank[3] = new Bank("9485", "0756788976522", 1000000L, "최정윤");
-        bank[4] = new Bank("2073", "3333072779383", 1000000L, "장민수");
+        bank[1] = new Bank("2680", "299559469724", 2000000L, "이승병");
+        bank[2] = new Bank("3606", "3920348293843", 3000000L, "박희진");
+        bank[3] = new Bank("9485", "0756788976522", 4000000L, "최정윤");
+        bank[4] = new Bank("2073", "3333072779383", 5000000L, "장민수");
 
         BankManager bm = new BankManager();
+
+        int cn;
+        cn = bm.logIn(bank, 5);
+
         do {
             Scanner sc = new Scanner(System.in);
             System.out.println("☆☆KH BANK 사용자 메뉴☆☆");
-
-            bm.logIn(bank, 5);
-
+            System.out.println("금액은 " + Integer.MAX_VALUE + "원 까지 입력 가능합니다.");
             System.out.println("1 >> 조회");
             System.out.println("2 >> 입금");
             System.out.println("3 >> 송금");
@@ -41,17 +40,16 @@ public class BankMenu {
             switch (select) {
 
                 case 1:
-                    bm.printInfo();
+                    bm.printInfo(bank, cn);
                     break;
                 case 2:
-                    //bm.deposit();
+                    bm.deposit(bank, cn);
                     break;
                 case 3:
-                    // bm.remittance() ;
-
+                    bm.remittance(bank, cn);
                     break;
                 case 4:
-                    //bm.withdrawal();
+                    bm.withdrawal(bank, cn);
                     break;
                 case 0:
                     System.out.println("프로그램을 종료합니다");
